07a8bdd677193e74f3e562acbe491c24b6f4e4b1
Adnane Drief <adnanedrief@gmail.com>
2021-09-28 17:47:51 +0100

    improved version

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const axios = require("axios");
 const express = require("express");
 const cheerio = require("cheerio");
 const fs = require("fs");
+const csvjson = require('csvjson');
+
 
 const PORT = 8000;
 const app = express();
@@ -15,11 +17,44 @@ function currentDateOfCheck() {
     let date = date_ob.getDate();
     let month = date_ob.getMonth() + 1;
     let year = date_ob.getFullYear();
+    let FullDate = date + "/" + month + "/" + year;
+    return FullDate;
+}
+
+function currentTimeOfCheck() {
+    let ts = Date.now();
+    let date_ob = new Date(ts);
     let hours = date_ob.getHours();
     let minutes = date_ob.getMinutes();
     let seconds = date_ob.getSeconds();
-    let FullDateandTime = "Date : " + date + "/" + month + "/" + year + ` ~ ~ Time : ${hours}:${minutes}:${seconds}`;
-    return FullDateandTime;
+    let FullTime = `${hours}:${minutes}:${seconds}`;
+    return FullTime;
+}
+
+function convertJSONToCSV() {
+    const readFile = fs.readFile;
+    const writeFile = fs.writeFile;
+    // Reading json file(filename -data.json)
+    readFile('./result.json', 'utf-8', (err, fileContent) => {
+        if (err) {
+            // Doing something to handle the error or just throw it
+            console.log(err);
+            throw new Error(err);
+        }
+        // Convert json to csv function
+        const csvData = csvjson.toCSV(fileContent, {
+            headers: 'key'
+        });
+        // Write data into csv file named college_data.csv
+        writeFile('./result.csv', csvData, (err) => {
+            if (err) {
+                console.log(err);
+                throw new Error(err);
+            }
+            console.log('Data stored into csv file successfully');
+        });
+    });
+
 }
 async function ScrapeData() {
     try {
@@ -33,11 +68,13 @@ async function ScrapeData() {
             const itemExtracted = {
                 name: '',
                 price: '',
-                date: ''
+                date: '',
+                time: ''
             }
             itemExtracted.name = $(this).find('.css-0').text();
             itemExtracted.price = $(this).find('.css-z3vg5b').text();
             itemExtracted.date = currentDateOfCheck();
+            itemExtracted.time = currentTimeOfCheck();
 
             // Ajouter à notre Array l'élément extrait
             ArrayOfResult.push(itemExtracted);
@@ -52,9 +89,11 @@ async function ScrapeData() {
             }
             console.log("Successfully written data to file");
         });
+
+        convertJSONToCSV();
     } catch (error) {
         console.log(error);
     }
 }
 
-setInterval(() => ScrapeData(), 2000);
+setInterval(() => ScrapeData(), 5000); // rexecute the script every 5 s
